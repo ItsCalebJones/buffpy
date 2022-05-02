531080f98e02f26fbe1b6902c6c91ae8800951ef
Caleb Jones <ca.jones9119@gmail.com>
2022-05-02 00:27:04 -0400

    Remove tests - bump python version

--- setup.py
+++ setup.py
@@ -2,17 +2,10 @@ from setuptools import setup
 
 
 requires_list = [
-    "certifi==2019.3.9",
-    "chardet==3.0.4",
     "colorama==0.4.1",
-    "coverage==4.5.3",
-    "httpretty==0.9.6",
-    "idna==2.8",
-    "mock==3.0.5",
-    "nose==1.3.7",
     "rauth==0.7.3",
     "requests==2.22.0",
-    "six==1.12.0",
+    "six",
 ]
 
 
@@ -28,6 +21,6 @@ setup(
     include_package_data=True,
     install_requires=requires_list,
     classifiers=[
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.10",
     ]
 )
